b31c2890c7aefc4fd5e6a7eb5859ca6eeffdddc8
1834902571 <66833207+1834902571@users.noreply.github.com>
2020-07-18 21:11:13 -0700

    Update 2.c

--- lab10/2.c
+++ lab10/2.c
@@ -1,195 +1,191 @@
-  #include <stdio.h>
-  #include <stdlib.h>
-
-  struct node {
-        int data;
-        struct node *prev, *next;
-  };
-
-  struct node *head = NULL, *tail = NULL;
-
-  struct node * createNode(int data) {
-        struct node *newnode = (struct node *)malloc(sizeof (struct node));
-        newnode->data = data;
-        newnode->next = newnode->prev = NULL;
-        return (newnode);
-  }
-  void createDeque();
-  void enqueueAtFront(int data);
-  void enqueueAtRear(int data);
-  void dequeueAtFront();
-  void dequeueAtRear() ;
-  void Size_of_Deque();
-  void isEmpty();
-  void display() ;
- int main() {
-        int data, ch;
-        createDeque();
-        while (1) {
-
-                printf("1. Puse at front\n2. Puse at rear\n");
-                printf("3. Pop at front\n4. Pop at rear\n");
-                printf("5. Size of Deque\n6. isEmpty of Not \n");
-                printf("7. Display\n8. Exit\n");
-                printf("Enter your choice:");
-                scanf("%d", &ch);
-                if(ch<=0||ch>8)
-                {
-                    printf("Invalid Input . Insert between 1 to 8 \n");
-                }
-                switch (ch) {
-                        case 1:
-                                printf("Enter the data to insert:");
-                                scanf("%d", &data);
-                                enqueueAtFront(data);
-                                break;
-
-                        case 2:
-                                printf("Enter ur data to insert:");
-                                scanf("%d", &data);
-                                enqueueAtRear(data);
-                                break;
-
-                        case 3:
-                                dequeueAtFront();
-                                break;
-
-                        case 4:
-                                dequeueAtRear();
-                                break;
-
-                        case 5:
-                                Size_of_Deque();
-                                break;
-
-                         case 6:
-                                isEmpty();
-                                break;
-
-                        case 7:
-                                display();
-                                break;
-
-
-                        case 8:
-                                exit(0);
-
-                        default:
-                                printf("Pls. enter correct option\n");
-                                break;
-                }
-
-        }system("cls");
-
-        return 0;
-  }
-
-  void createDeque() {
-        head = createNode(0);
-        tail = createNode(0);
-        head->next = tail;
-        tail->prev = head;
-  }
-
-  /* insertion at the front of the queue */
-  void enqueueAtFront(int data) {
-        struct node *newnode, *temp;
-        newnode = createNode(data);
-        temp = head->next;
-        head->next = newnode;
-        newnode->prev = head;
-        newnode->next = temp;
-        temp->prev = newnode;
-  }
-
-  /*insertion at the rear of the queue */
-  void enqueueAtRear(int data) {
-        struct node *newnode, *temp;
-        newnode = createNode(data);
-        temp = tail->prev;
-        tail->prev = newnode;
-        newnode->next = tail;
-        newnode->prev = temp;
-        temp->next = newnode;
-  }
-
-  /* deletion at the front of the queue */
-  void dequeueAtFront() {
-        struct node *temp;
-        if (head->next == tail) {
-                printf("Queue is empty\n");
-        } else {
-                temp = head->next;
-                head->next = temp->next;
-                temp->next->prev = head;
-                free(temp);
+#include<stdio.h>
+#include<stdlib.h>
+#include "DoublyNonCircular.h"
+void display(head);
+node* push(node *head);
+node* pop(node *head);
+node* other_operations(node *head);
+int main()
+{
+    int op;
+    node *head = NULL;
+    //head = create_list();
+    do
+    {
+        system("cls");
+        printf("Select Your Option: \n");
+        printf("1. DISPLAY\n");
+        printf("2. PUSH\n");
+        printf("3. POP\n");
+        printf("4. OTHER OPERATIONS\n") ;
+        printf("5. Close\n");
+        printf("Enter Your Option Number: ");
+        scanf("%d", &op);
+        if(op<=0 || op>5)
+        {
+            printf("Invalid Option Number. Insert between 1 to 5\n");
         }
-        return;
-  }
-
-
-  /* deletion at the rear of the queue */
-
-  void dequeueAtRear()  {
-        struct node *temp;
-        if (tail->prev == head) {
-                printf("Queue is empty\n");
-        } else {
-                temp = tail->prev;
-                tail->prev = temp->prev;
-                temp->prev->next = tail;
-                free(temp);
+        else if(op == 1)
+        {
+            printf("Linked List: ");
+            display(head);
         }
-        return;
-  }
-    /* This deque isEmpty or Not*/
-   void isEmpty() {
-        struct node *temp;
-
-        if (head->next == tail) {
-                printf("Queue is empty\n");
-                return;
-        }else{
-          printf("Queue is Not empty\n");
+        else if(op == 2)
+        {
+            head = push(head);
         }
-   }
-    /* Fine the Size of this deque */
-   void  Size_of_Deque() {
-        struct node *temp;
-        int Count=0;
-        if (head->next == tail) {
-                printf("Queue is empty\n");
-                return;
+        else if(op == 3)
+        {
+            head = pop(head);
         }
-
-        temp = head->next;
-        while (temp != tail) {
-                Count++;
-                temp = temp->next;
+        else if(op == 4)
+        {
+            other_operations(head);
         }
-        printf("Size of this Queue : %d\n",Count);
-
-  }
-
-  /* display elements present in the queue */
-  void display() {
-        struct node *temp;
-
-        if (head->next == tail) {
-                printf("Queue is empty\n");
-                return;
+    }
+    while(op!=5);
+}
+void display(node *head)
+{
+    display_list(head);
+    getchar();
+    printf("Press Enter to Continue");
+    getchar();
+};
+node* push(node *head)
+{
+    int op, value;
+    do
+    {
+        system("cls");
+        printf("Select Your Option: \n");
+        printf("1. Push Front\n");
+        printf("2. Push Back\n");
+        printf("3. Back to Main Menu\n");
+        printf("Enter Your Option Number: ");
+        scanf("%d", &op);
+        if(op<=0 || op>3)
+        {
+            printf("Invalid Option Number. Insert between 1 to 3\n");
         }
-
-        temp = head->next;
-        while (temp != tail) {
-                printf("%-3d", temp->data);
-                temp = temp->next;
+        else if(op<3)
+        {
+            printf("Input Value to PUSH: ");
+            scanf("%d", &value);
+            if(op==1)
+            {
+                return push_front(head, value);
+            }
+            else if(op==2)
+            {
+                return push_back(head, value);
+            }
         }
-        printf("\n");
-  }
-
-
-
-
-
-
+    }
+    while(op!=3);
+    return head;
+};
+node* pop(node *head)
+{
+    int op, value;
+    do
+    {
+        system("cls");
+        printf("Select Your Option: \n");
+        printf("1. Pop Front\n");
+        printf("2. Pop Back\n");
+        printf("3. Clear\n");
+        printf("4. Back to Main Menu\n");
+        printf("Enter Your Option Number: ");
+        scanf("%d", &op);
+        if(op<=0 || op>4)
+        {
+            printf("Invalid Option Number. Insert between 1 to 4\n");
+        }
+        else if(op<4)
+        {
+            if(op==1)
+            {
+                return pop_front(head);
+            }
+            else if(op==2)
+            {
+                return pop_back(head);
+            }
+            else if(op==3)
+            {
+                return clear(head);
+            }
+        }
+    }
+    while(op!=4);
+    return head;
+};
+node* other_operations(node *head)
+{
+    int op;
+    do
+    {
+        system("cls");
+        printf("Select Your Option: \n");
+        printf("1. Head\n");
+        printf("2. Tail\n");
+        printf("3. Empty\n");
+        printf("4. Size\n") ;
+        printf("5. Front\n") ;
+        printf("6. Back\n") ;
+        printf("7. Back to Main Menu\n");
+        printf("Enter Your Option Number: ");
+        scanf("%d", &op);
+        if(op<=0 || op>7)
+        {
+            printf("Invalid Option Number. Insert between 1 to 7\n");
+        }
+        else if(op<7)
+        {
+            if(op==1)
+            {
+                printf("Head: %d\n", front(head));
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+            else if(op==2)
+            {
+                printf("Tail: %d\n", back(head));
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+            else if(op==3)
+            {
+                empty(head);
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+            else if(op==4)
+            {
+                size(head);
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+            else if(op==5)
+            {
+                printf("Front: %d\n",front(head));
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+            else if(op==6)
+            {
+                printf("Back: %d\n", back(head));
+                getchar();
+                printf("Press Enter to Continue");
+                getchar();
+            }
+        }
+    }
+    while(op!=7);
+};
