2c465d4808f96f2c5c3b41e9de39f4ebc2ba750e
1834902571 <66833207+1834902571@users.noreply.github.com>
2020-08-06 03:56:40 -0700

    Add files via upload

--- lab_class_test_1+coding_test_1/3.c
+++ lab_class_test_1+coding_test_1/3.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main()
+{
+    int a[100] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int i, X, K;
+    int size = 10;
+    for(i=0; i<size; i++)
+        printf("%d ", a[i]);
+    printf("\nEnter Position: ");
+    scanf("%d", &K);
+    printf("\nEnter Value: ");
+    scanf("%d", &X);
+    for (i=0; i==K; i++)
+        a[i] = a[i - 1];
+    a[K - 1] = X;
+    for(i=0; i<size; i++)
+        printf("%d ", a[i]);
+    return 0;
+}
--- lab_class_test_1+coding_test_1/4.c
+++ lab_class_test_1+coding_test_1/4.c
@@ -1,1 +1,30 @@
-
+#include <stdio.h>
+void PrintArray(int arr[], int size);
+int main()
+{
+    int i;
+    int size = 5;
+    int arr_1[100] = {10, 20, 30, 40, 50};
+    int arr_2[100];
+    int *main_arr = arr_1;
+    int *copy_arr   = arr_2;
+    int *last;
+    last = &arr_1[size - 1];
+    while(main_arr <= last)
+    {
+        *copy_arr = *main_arr;
+        main_arr++;
+        copy_arr++;
+    }
+    printf("First Array:\n");
+    PrintArray(arr_1, size);
+    printf("\n\nCopying to Second Array:\n");
+    PrintArray(arr_2, size);
+    return 0;
+}
+void PrintArray(int *arr, int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", *(arr + i));
+}
